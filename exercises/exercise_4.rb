88e058e7097cd44d92f2ffcc918648b7cccea082
Sam <samuelrush@gmail.com>
2019-12-10 01:46:04 +0000

    mens store filter with select statements for ex4

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,13 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+user = Store.create(name: "Surrey", annual_revenue: 224000, mens_apparel: 0, womens_apparel: 1)
+user = Store.create(name: "Whistler", annual_revenue: 1900000, mens_apparel: 1, womens_apparel: 0)
+user = Store.create(name: "Yaletown", annual_revenue: 430000, mens_apparel: 1, womens_apparel: 1)
+
+@mens_stores = Store.where(mens_apparel: true)
+@mens_stores.each do |store|
+  puts "#{store[:name]} has an annual revenue of #{store[:annual_revenue]}"
+end
+
+@womens_stores = Store.where(womens_apparel: true, annual_revenue: < 1000000)
